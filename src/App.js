ed6ee25a59415e5723c96c48028e3dc532d64946
leojuriolli7 <leojuriolli@gmail.com>
2022-02-27 23:27:57 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { api } from "./services/api";
+import { Spinner, Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const [dogPhoto, setDogPhoto] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const getDog = () => {
+    api.get("breeds/image/random").then((response) => {
+      setDogPhoto(response.data.message);
+      setLoading(false);
+    });
+  };
+
+  const randomizeDogPhoto = () => {
+    setLoading(true);
+    getDog();
+  };
+
+  useEffect(() => {
+    getDog();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Dog Randomizer</h1>
+      <Button variant="light" onClick={randomizeDogPhoto} disabled={loading}>
+        {" "}
+        Randomize dog{" "}
+      </Button>
+
+      {loading && <Spinner animation="border" variant="light" size="sm" />}
+
+      {dogPhoto && <img className="dog" src={dogPhoto} alt="Dog" />}
     </div>
   );
 }
